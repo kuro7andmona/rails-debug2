79686c7bf282be196fdb817ce777725cdbdc09ef
kuro7andmona <129634798+kuro7andmona@users.noreply.github.com>
2023-06-05 08:58:28 +0000

    検索機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -10,13 +10,15 @@ Rails.application.routes.draw do
   resource :favorites, only: [:create, :destroy]
   resources :book_comments, only: [:create, :destroy]
   end
-
+ get 'search' => 'searches#search'
   resources :users, only: [:index,:show,:edit,:update]
   resources :users do
     resource :relationships, only: [:create, :destroy]
     get 'followings' => 'relationships#followings', as: 'followings'
     get 'followers' => 'relationships#followers' , as: 'followers'
   end
+  #get 'search' => 'searches#search'
+
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
